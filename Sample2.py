e8615a4324b0a1979689b8489cecea5435ecb4d9
RashmiJose <rashmijose92@gmail.com>
2020-12-30 00:31:43 +0530

    Adding changes

--- Sample2.py
+++ Sample2.py
@@ -1,17 +1,11 @@
 pipeline {
-	agent any
-	stages {
-		stage('Design') {
-		    print 'Build step is executed successfully'
-             	}
-		stage('Build') {
-		    print 'Build step is executed successfully'
-             	}
-		stage('Test') {
-		    print 'Build step is executed successfully'
-             	}
-		stage('Deploy') {
-		    print 'Build step is executed successfully'
-             	}
-	}
-   }
+    agent any
+
+    stages {
+        stage('Design') {
+            steps {
+                print("Hello World")
+            }
+        }
+    }
+}
